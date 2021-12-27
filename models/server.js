70bf37d1aafad046fdcf240ab58a4ec2c8675cc6
Julio Zaravia <julio.zaravia@chicho.pe>
2021-12-27 17:38:15 -0500

    ADD CORS middleware

--- models/server.js
+++ models/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 class Server {
   constructor() {
@@ -11,6 +12,7 @@ class Server {
   }
 
   middlewares() {
+    this.app.use(cors());
     this.app.use(express.static('public'));
   }
 
